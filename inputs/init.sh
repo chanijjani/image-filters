e302f02d99a84b0a44130965b6c25b20b2b26573
Denis <denis.hoornaert@tum.de>
2022-08-13 19:34:43 +0200

    input inages generation: auto deduce script location

--- inputs/init.sh
+++ inputs/init.sh
@@ -1,16 +1,19 @@
+# get script path
+SCRIPT_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+
 # get image and rename
-wget https://www.nasa.gov/images/content/569718main_fd3predock_full.jpg
-mv 569718main_fd3predock_full.jpg 4k.jpg
+wget https://www.nasa.gov/images/content/569718main_fd3predock_full.jpg --directory-prefix=${SCRIPT_DIR} --quiet
+mv ${SCRIPT_DIR}/569718main_fd3predock_full.jpg ${SCRIPT_DIR}/4k.jpg
 
 # convert to BMP
-convert 4k.jpg 4k.bmp
-rm 4k.jpg
+convert ${SCRIPT_DIR}/4k.jpg ${SCRIPT_DIR}/4k.bmp
+rm ${SCRIPT_DIR}/4k.jpg
 
 # resize BMP image
-convert 4k.bmp -resize 1920x1080 fullhd.bmp
-convert 4k.bmp -resize 640x480 vga.bmp
-convert 4k.bmp -resize 88x66 sqcif.bmp
-convert 4k.bmp -resize 48x36 sim_fast.bmp
-convert 4k.bmp -resize 88x66 sim.bmp
-convert 4k.bmp -resize 176x132 qcif.bmp
-convert 4k.bmp -resize 352x264 cif.bmp
+convert ${SCRIPT_DIR}/4k.bmp -resize 1920x1080 ${SCRIPT_DIR}/fullhd.bmp
+convert ${SCRIPT_DIR}/4k.bmp -resize 640x480 ${SCRIPT_DIR}/vga.bmp
+convert ${SCRIPT_DIR}/4k.bmp -resize 88x66 ${SCRIPT_DIR}/sqcif.bmp
+convert ${SCRIPT_DIR}/4k.bmp -resize 48x36 ${SCRIPT_DIR}/sim_fast.bmp
+convert ${SCRIPT_DIR}/4k.bmp -resize 88x66 ${SCRIPT_DIR}/sim.bmp
+convert ${SCRIPT_DIR}/4k.bmp -resize 176x132 ${SCRIPT_DIR}/qcif.bmp
+convert ${SCRIPT_DIR}/4k.bmp -resize 352x264 ${SCRIPT_DIR}/cif.bmp
