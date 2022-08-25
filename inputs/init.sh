6dce15495ccf5d5e4b1745194cf793dd7d960337
Denis <denis.hoornaert@tum.de>
2022-08-25 15:04:56 +0200

    inputs: setup script: remove only fetching: uses local jpg image

--- inputs/init.sh
+++ inputs/init.sh
@@ -1,13 +1,8 @@
 # get script path
 SCRIPT_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 
-# get image and rename
-wget https://www.nasa.gov/images/content/569718main_fd3predock_full.jpg --directory-prefix=${SCRIPT_DIR} --quiet
-mv ${SCRIPT_DIR}/569718main_fd3predock_full.jpg ${SCRIPT_DIR}/4k.jpg
-
 # convert to BMP
 convert ${SCRIPT_DIR}/4k.jpg ${SCRIPT_DIR}/4k.bmp
-rm ${SCRIPT_DIR}/4k.jpg
 
 # resize BMP image
 convert ${SCRIPT_DIR}/4k.bmp -resize 1920x1080 ${SCRIPT_DIR}/fullhd.bmp
